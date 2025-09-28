169b12f5d7ecfa2cd1b304f124f379ae838f2985
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2025-09-28 18:56:42 -0300

    fix:fixing vercel deploy error part 2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import { useFlags } from "flagsmith/react";
 
 export default function Page() {
-  const flags = useFlags(["configure_card_controller"]); // only causes re-render if specified flag values / traits change
+  const flags = useFlags(["configure_card_controller"]);
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Provider from "./provider";
+import flagsmith from "flagsmith/isomorphic";
 import "./globals.css";
+
+export const dynamic = "force-dynamic";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,12 +26,21 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const flagsmithState = await flagsmith
+    .init({
+      environmentID: process.env.Flagsmith,
+      identity: "test_user_id",
+    })
+    .then(() => {
+      return flagsmith.getState();
+    });
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Provider flagsmithState={flagsmithState}>{children}</Provider>
       </body>
     </html>
   );
